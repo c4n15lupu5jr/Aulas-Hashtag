b4f345d607004a06b4b8342a68beb3b195834d9c
c4n15lupu5jr <84355075+c4n15lupu5jr@users.noreply.github.com>
2023-03-06 18:57:59 -0300

    Mais JavaScript

--- TOCADOR_MUSICA/script.js
+++ TOCADOR_MUSICA/script.js
@@ -5,11 +5,13 @@ const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const currentProgress = document.getElementById('current-progress');
+const progressContainer = document.getElementById('progress-container');
 
 const AbsoluteTerritory = {
     songName : 'Absolute Territory',
     artist : 'Ken Ashcorp',
-    file: 'Ken Ashcorp - Absolute Territory'
+    file: 'AbsoluteTerritory'
 };
 
 const Musica2 = {
@@ -79,8 +81,24 @@ function nextSong(){
     playSong();
 }
 
+function updateProgressBar(){
+    song.currentTime
+    song.duration
+    const barWidth = (song.currentTime/song.duration)*100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+}
+
+function jumpTo(event){
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition/width)* song.duration;
+    song.currentTime = jumpToTime;
+}
+
 initializeSong();
 
 play.addEventListener('click' , playPauseDecider);
-previous.addEventListener('click' , previousSong)
-next.addEventListener('click' , nextSong)
+previous.addEventListener('click' , previousSong);
+next.addEventListener('click' , nextSong);
+song.addEventListener('timeupdate' , updateProgressBar);
+progressContainer.addEventListener('click' , jumpTo)
